c574e6d1e8ef91342ab1a6707a9ddebe37d41a46
Brandon Cho <hjun1114@gmail.com>
2021-09-29 00:02:20 -0700

    adding simple interface logic to communicate ethereum network

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manager: "",
+      players: [],
+      balance: "",
+      value: "",
+      status: "",
+    };
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
+  }
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ status: "validating on Ethereum network..." });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+
+    this.setState({ status: "Success!" });
+  };
+  pickWinner = async () => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ status: "validating on Ethereum network..." });
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+    this.setState({ status: "Success!" });
+  };
+  render() {
+    return (
+      <div>
+        <h2> Prize Pool App </h2>
+        <h3>
+          {" "}
+          This application communicates Metamask to get your address info.
+          Please install it in your browser.
+        </h3>
+        <p> Contract Owner: {this.state.manager} </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Currently {this.state.players.length} player(s) have been
+          participated.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          Total amount of collected moeny is:{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")}
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h3>
+            * If you want to participate, you need to submit minimum of 0.01
+            ether.
+          </h3>
+          <div>
+            <label htmlFor="">Amount of ether you want to put in </label>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={(event) => {
+                this.setState({ value: event.target.value });
+              }}
+            />
+          </div>
+          <button>Submit</button>
+        </form>
+        <hr />
+        <h3>Pick a winner</h3>
+        <button onClick={this.pickWinner}></button>
+        <hr />
+        <h2>{this.state.status}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,61 @@
+import web3 from "./web3";
+
+const address = "0xc5e5E265fDB918EF2d077aa0dd2c23669a5ce251";
+const ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+// create local contract instance that is a representational object of a real contract in Rinkeby network.
+// NOT the actual object from the blockchain network!
+
+export default new web3.eth.Contract(ABI, address);
